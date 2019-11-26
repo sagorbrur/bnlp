d6221603a7d7761d1bb9f273c6430f470a26283e
Md Ibrahim <ibrahimriad11@gmail.com>
2019-11-26 23:29:07 +0600

    Added auto download of pretrained model and vocab

--- bnlp/sentencepiece_tokenizer.py
+++ bnlp/sentencepiece_tokenizer.py
@@ -1,6 +1,46 @@
 import os
 import sentencepiece as bsp
+import urllib.request
+import sys
+import time
 
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024 * duration))
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
+                    (percent, progress_size / (1024 * 1024), speed, duration))
+    sys.stdout.flush()
+    
+    
+def check_model(model_path):
+    pretrained_model_url = "https://github.com/iriad11/bnlp/raw/master/model/bn_spm.model"
+    pretrained_vocab_url = "https://github.com/iriad11/bnlp/raw/master/model/bn_spm.vocab"
+    if model_path[0] == "/":
+        model_path = model_path[:2].replace("/","./") + model_path[2:]
+
+    if not os.path.exists(model_path):
+        os.makedirs(os.path.dirname(model_path),exist_ok=True)
+        print('Downloading pretrained model...')
+        urllib.request.urlretrieve(pretrained_model_url, model_path, reporthook)
+        print('\nDownload completed.\n')
+        
+    mod = os.path.dirname(model_path)
+    vocab_path = "." + mod + "/bn_spm.vocab"
+    
+    if not os.path.exists(vocab_path):
+        os.makedirs(os.path.dirname(vocab_path),exist_ok=True)
+        print('Downloading pretrained vocab...')
+        urllib.request.urlretrieve(pretrained_vocab_url, vocab_path, reporthook)
+        print('\nDownload completed.\n')
+        
+    return model_path
+    
 
 class SP_Tokenizer(object):
     def __init__(self, is_train=False):
@@ -31,10 +71,10 @@ class SP_Tokenizer(object):
 
         """
         model = bsp.SentencePieceProcessor()
+        model_path = check_model(model_path)
         model.Load(model_path)
         tokens = model.EncodeAsPieces(text)
 
         return tokens
-
 
 
