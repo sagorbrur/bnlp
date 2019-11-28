bc2e9a17e7aac811337e8e8e84f3aef10b5ab3f8
Sagor Sarker <sagorhem3532@gmail.com>
2019-11-28 21:06:31 +0600

    Update basic_tokenizer.py

--- bnlp/basic_tokenizer.py
+++ bnlp/basic_tokenizer.py
@@ -112,18 +112,7 @@ class BasicTokenizer(object):
 
     return ["".join(x) for x in output]
 
-  def _tokenize_chinese_chars(self, text):
-    """Adds whitespace around any CJK character."""
-    output = []
-    for char in text:
-      cp = ord(char)
-      if self._is_chinese_char(cp):
-        output.append(" ")
-        output.append(char)
-        output.append(" ")
-      else:
-        output.append(char)
-    return "".join(output)
+ 
   
 
 if __name__ == "__main__":
