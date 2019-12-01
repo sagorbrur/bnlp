a0794b7b9e62abf602828aaf97e4d023f4efe9e2
Sagor Sarker <sagorhem3532@gmail.com>
2019-12-01 12:53:06 +0600

    Update __init__.py

--- bnlp/__init__.py
+++ bnlp/__init__.py
@@ -1,5 +1,8 @@
 import os
 from bnlp.sentencepiece_tokenizer import SP_Tokenizer
 from bnlp.nltk_tokenizer import NLTK_Tokenizer
+from bnlp.basic_tokenizer import BasicTokenizer
 from bnlp.bengali_word2vec import Bengali_Word2Vec
 from bnlp.bengali_fasttext import Bengali_Fasttext
+
+__version__="1.1.0"
