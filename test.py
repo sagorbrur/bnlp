c948ad2f14c77ed9bacb645a050168b708cf003c
Sagor Sarker <sagorhem3532@gmail.com>
2019-12-02 11:39:58 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import unittest
+from bnlp.sentencepiece_tokenizer import SP_Tokenizer
+from bnlp.nltk_tokenizer import NLTK_Tokenizer
 from bnlp.basic_tokenizer import BasicTokenizer
+from bnlp.bengali_word2vec import Bengali_Word2Vec
+from bnlp.bengali_fasttext import Bengali_Fasttext
 
 class TestBNLP(unittest.TestCase):
 
