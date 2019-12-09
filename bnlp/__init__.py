009ecf2fceda32ff5c019d08f2aee7e2ab9c3fb8
Sagor Sarker <sagorhem3532@gmail.com>
2019-12-09 21:14:37 +0600

    Update __init__.py

--- bnlp/__init__.py
+++ bnlp/__init__.py
@@ -7,7 +7,7 @@ from bnlp.nltk_tokenizer import NLTK_Tokenizer
 from bnlp.basic_tokenizer import BasicTokenizer
 from bnlp.bengali_word2vec import Bengali_Word2Vec
 from bnlp.bengali_fasttext import Bengali_Fasttext
-from bnlp.glove_wordvector.py import BN_Glove
+from bnlp.glove_wordvector import BN_Glove
 
 
 
