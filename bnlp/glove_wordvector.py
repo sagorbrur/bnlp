f5636d7a78f156f4481ce3446b313cbd83713a62
Sagor Sarker <sagorhem3532@gmail.com>
2019-12-09 21:22:06 +0600

    Update glove_wordvector.py

--- bnlp/glove_wordvector.py
+++ bnlp/glove_wordvector.py
@@ -1,7 +1,10 @@
 import numpy as np
+import scipy
 from scipy import spatial
-# print(np.__version__) 1.17.4
-# print(scipy.__version__)  1.3.3
+#print(np.__version__) #1.17.4
+#print(scipy.__version__) #1.3.3
+
+
 
 class BN_Glove(object):
   
@@ -16,7 +19,7 @@ class BN_Glove(object):
       result_vec = embeddings_dict[test_word]
       return result_vec
 
-  def closest_word(self,glove_path):
+  def closest_word(self,glove_path, test_word):
 
     def find_closest_embeddings(embedding):
       return sorted(embeddings_dict.keys(), key=lambda word: spatial.distance.euclidean(embeddings_dict[word], embedding))
@@ -29,14 +32,15 @@ class BN_Glove(object):
             word = values[0]
             vector = np.asarray(values[1:], "float32")
             embeddings_dict[word] = vector
-    result = find_closest_embeddings(embeddings_dict["গ্রাম"])[:10]
+    result = find_closest_embeddings(embeddings_dict[test_word])[:10]
     return result
 
 
 if __name__=="__main__":
     glove_path = "bn_glove.39M.100d.txt"
+    word = "গ্রাম"
     bng = BN_Glove()
-    res = bng.closest_word(glove_path)
+    res = bng.closest_word(glove_path, word)
     print(res)
-    vec = bng.word2vec(glove_path, "গ্রাম")
+    vec = bng.word2vec(glove_path, word)
     print(vec)
