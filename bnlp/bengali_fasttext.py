a0800688111ed613665d21f2f15ae4358eac5bee
Sagor Sarker <sagorhem3532@gmail.com>
2019-12-14 09:41:46 +0600

    Update bengali_fasttext.py

--- bnlp/bengali_fasttext.py
+++ bnlp/bengali_fasttext.py
@@ -5,9 +5,9 @@ class Bengali_Fasttext(object):
     def __init__(self, is_train=False):
         self.is_train = is_train
 
-    def train_fasttext(self, data, model_name):
+    def train_fasttext(self, data, model_name, epoch):
         if self.is_train:
-            model = fasttext.train_unsupervised(data, model='skipgram', minCount=1)
+            model = fasttext.train_unsupervised(data, model='skipgram', minCount=1, epoch=epoch)
             model.save_model(model_name)
 
     def generate_word_vector(self, model_path, word):
@@ -15,3 +15,10 @@ class Bengali_Fasttext(object):
         word_vector = model[word]
 
         return word_vector
+    
+    
+  if __name__ == "__main__":
+    bft = Bengali_Fasttext(is_train=True)
+    data = "data.txt"
+    model_name = "saved_model.bin"
+    bft.train_fasttext(data, model_name, epoch=10)
