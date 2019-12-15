b51613028929212ffdf8b1f128f549199e207885
Sagor Sarker <sagorhem3532@gmail.com>
2019-12-15 15:33:37 +0600

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from bnlp.nltk_tokenizer import NLTK_Tokenizer
 from bnlp.basic_tokenizer import BasicTokenizer
 from bnlp.bengali_word2vec import Bengali_Word2Vec
 from bnlp.bengali_fasttext import Bengali_Fasttext
+from bnlp.bengali_pos import BN_CRF_POS
 
 class TestBNLP(unittest.TestCase):
 
