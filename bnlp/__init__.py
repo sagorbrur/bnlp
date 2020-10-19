5381b92c2265c9ca4372b15b4e3f04a60fc27c4b
sagorbrur <sagorhem3532@gmail.com>
2020-10-19 20:35:06 +0600

    dev1 ready for build

--- bnlp/__init__.py
+++ bnlp/__init__.py
@@ -1,4 +1,4 @@
-__version__="3.0"
+__version__="3.0.0.dev1"
 
 
 import os
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="bnlp_toolkit",
-    version="3.0",
+    version="3.0.0.dev1",
     author="Sagor Sarker",
     author_email="sagorhem3532@gmail.com",
     description="BNLP is a natural language processing toolkit for Bengali Language",
