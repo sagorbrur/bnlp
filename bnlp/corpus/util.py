301f78ed1457bfc77b8a6b5252ae0f0c1481fe4c
sagorbrur <sagorhem3532@gmail.com>
2021-03-12 07:27:34 +0600

    Revert "added remove_foreign_words function under corpus class"
    This reverts commit 9a1da9e843dc78ed46a1b7879b7e5417930b2ff3.

--- bnlp/corpus/util.py
+++ bnlp/corpus/util.py
@@ -1,38 +1,20 @@
 from bnlp.tokenizer.basic import BasicTokenizer
-from bnlp.corpus import punctuations
 
 
 def remove_stopwords(text, stopwords):
-  """This function remove stopwords from Bengali text
+  """
+  This function remove stopwords from text
+  parameters:
+    text: str
+    stopwords: list
+  return: tokens of word without stopwords
 
-  Args:
-      text (str): input text
-      stopwords (list): stopword list
-
-  Returns:
-      list: list of words without stopwords
   """
   tokenizer = BasicTokenizer()
   words = tokenizer.tokenize(text)
   filtered_words = [w for w in words if not w in stopwords]
   return filtered_words
 
-def remove_foreign_words(text):
-  """This function removes foreign words from Bengali text
-     formula copied from this stackoverflow answers
-     https://stackoverflow.com/questions/64433299/how-can-i-remove-foreign-word-from-bengali-text-in-python
 
-  Args:
-      text (str): input text
-
-  Returns:
-      str: output text
-  """
-  text = "".join(i for i in text if i in punctuations or 2432 <= ord(i) <= 2559 or ord(i)== 32)
-  tokens = text.split()
-  # removing extra punctions from tokens
-  tokens = [token for token in tokens if token not in punctuations]
-  result_text = " ".join(tokens)
-  return result_text
 
 
