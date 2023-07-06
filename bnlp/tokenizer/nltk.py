5c1185f1a7ff5ae46c695515161735011d7d3d47
sagorbrur <sagorhem3532@gmail.com>
2023-07-06 20:14:01 +0600

    update nltk word tokenizer with dummy token replacement method

--- bnlp/tokenizer/nltk.py
+++ bnlp/tokenizer/nltk.py
@@ -8,27 +8,28 @@ except LookupError:
     nltk.download("punkt")
 
 
+DUMMYTOKEN = "<dummy_bangla_token>"
+
 class NLTKTokenizer:
     def word_tokenize(self, text: str) -> List[str]:
+        text = text.replace(".", DUMMYTOKEN)  # to deal with abbreviations
+        text = text.replace("।", ".")
         tokens = nltk.word_tokenize(text)
         new_tokens = []
         for token in tokens:
-            if token[-1] == "।" and len(token) > 1:
-                token_1 = token[:-1]
-                token_2 = token[-1]
-                new_tokens.append(token_1)
-                new_tokens.append(token_2)
-            else:
-                new_tokens.append(token)
+            token = token.replace(".", "।")  # do operation in reverse order
+            token = token.replace(DUMMYTOKEN, ".")
+            new_tokens.append(token)
+
         return new_tokens
 
     def sentence_tokenize(self, text: str) -> List[str]:
-        text = text.replace(".", "<dummy_bangla_token>")  # to deal with abbreviations
+        text = text.replace(".", DUMMYTOKEN)  # to deal with abbreviations
         text = text.replace("।", ".")
         tokens = nltk.tokenize.sent_tokenize(text)
         new_tokens = []
         for token in tokens:
             token = token.replace(".", "।")  # do operation in reverse order
-            token = token.replace("<dummy_bangla_token>", ".")
+            token = token.replace(DUMMYTOKEN, ".")
             new_tokens.append(token)
         return new_tokens
