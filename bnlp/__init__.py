3dad097c3741958a9eacf52808a025983f00aeef
sagorbrur <sagorhem3532@gmail.com>
2023-07-10 11:29:14 +0600

    add version incompatibality warning in present package

--- bnlp/__init__.py
+++ bnlp/__init__.py
@@ -1,5 +1,4 @@
-__version__ = "3.3.1"
-
+__version__ = "3.3.2"
 
 import os
 from bnlp.pos import POS
@@ -11,3 +10,9 @@ from bnlp.embedding.word2vec import BengaliWord2Vec
 from bnlp.embedding.glove import BengaliGlove
 from bnlp.embedding.doc2vec import BengaliDoc2vec
 from bnlp.cleantext.clean import CleanText
+
+import logging
+
+logging.warning(f"""The current version ({__version__}) of bnlp will not be compatible with the upcoming release.
+If you are using version <={__version__} please specify bnlp_toolkit with exact version, otherwise it will raises error in the upcoming version. 
+To migrate feel free to checkout the newer version (4.0.0). It will release soon as beta.""")
