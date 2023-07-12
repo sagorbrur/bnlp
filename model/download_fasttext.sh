582c2930fe6cc9ccf1b1493353852a2d508f6286
sagorbrur <sagorhem3532@gmail.com>
2023-07-12 23:51:23 +0600

    update model download bash file

---
+++ model/download_fasttext.sh
@@ -0,0 +1,4 @@
+wget https://huggingface.co/sagorsarker/bangla-fasttext/resolve/main/bengali_fasttext_wiki.zip
+unzip bengali_fasttext_wiki.zip
+rm -rf bengali_fasttext_wiki.zip
+echo "Model download and extract has completed!"
---
+++ model/download_word2vec.sh
@@ -0,0 +1,4 @@
+wget https://huggingface.co/sagorsarker/bangla_word2vec/resolve/main/bangla_word2vec_gen4.zip
+unzip bangla_word2vec_gen4.zip
+rm -rf bangla_word2vec_gen4.zip
+echo "Model download and extract has completed!"
