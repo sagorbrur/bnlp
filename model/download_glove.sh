0b334e161b3339de12ede70647a216c2d6a1148a
sagorbrur <sagorhem3532@gmail.com>
2023-07-13 00:46:53 +0600

    update glove vector download bash file

---
+++ model/download_glove.sh
@@ -0,0 +1,4 @@
+wget https://huggingface.co/sagorsarker/bangla-glove-vectors/resolve/main/bn_glove.39M.100d.zip
+unzip bn_glove.39M.100d.zip
+rm -rf bn_glove.39M.100d.zip
+echo "Model download and extract has completed!"
