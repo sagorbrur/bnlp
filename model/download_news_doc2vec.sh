7f02e6b647f29d3cd73f9a5cb47405a22933e73b
sagorbrur <sagorhem3532@gmail.com>
2023-07-12 23:16:49 +0600

    update doc2vec download bash file

---
+++ model/download_news_doc2vec.sh
@@ -0,0 +1,4 @@
+wget https://huggingface.co/sagorsarker/news_article_doc2vec/resolve/main/news_article_doc2vec.zip
+unzip news_article_doc2vec.zip
+rm -rf news_article_doc2vec.zip
+echo "Model download and extract has completed!"
---
+++ model/download_wiki_doc2vec.sh
@@ -0,0 +1,4 @@
+wget https://huggingface.co/sagorsarker/bnwiki_doc2vec_model/resolve/main/bnwiki_doc2vec_model.zip
+unzip bnwiki_doc2vec_model.zip
+rm -rf bnwiki_doc2vec_model.zip
+echo "Model download and extract has completed!"
