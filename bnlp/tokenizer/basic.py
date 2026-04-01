3a4f92279ecc3824ef3606fc9cfa60c1e5e3c67e
sagorbrur <sagorhem3532@gmail.com>
2026-04-01 20:37:12 +0600

    Remove six dependency and Python 2 compatibility code from basic tokenizer

--- bnlp/tokenizer/basic.py
+++ bnlp/tokenizer/basic.py
@@ -4,28 +4,17 @@ Basic tokenization tokenize sentence using white spaces, punctuation mark
 Code shamelessly copied from BERT tokenization
 To check Original Code: https://github.com/google-research/bert/blob/master/tokenization.py
 """
-import six
 import unicodedata
 from typing import List
 
 def convert_to_unicode(text):
     """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
-    if six.PY3:
-        if isinstance(text, str):
-            return text
-        elif isinstance(text, bytes):
-            return text.decode("utf-8", "ignore")
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
-    elif six.PY2:
-        if isinstance(text, str):
-            return text.decode("utf-8", "ignore")
-        elif isinstance(text, unicode):
-            return text
-        else:
-            raise ValueError("Unsupported string type: %s" % (type(text)))
+    if isinstance(text, str):
+        return text
+    elif isinstance(text, bytes):
+        return text.decode("utf-8", "ignore")
     else:
-        raise ValueError("Not running on Python2 or Python 3?")
+        raise ValueError("Unsupported string type: %s" % (type(text)))
 
 
 def whitespace_tokenize(text: str) -> List[str]:
